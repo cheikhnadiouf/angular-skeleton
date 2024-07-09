f09da1e44eeaf55a4980788ff10217e7a7feb4b7
Cheikhna Diouf <contact@cheikhnadiouf.com>
2024-07-09 06:55:05 +0200

    Update: Enhanced reactive form with Unified Control State Change Events

--- src/app/todos/todo/todo.component.ts
+++ src/app/todos/todo/todo.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, OnDestroy, AfterViewInit, inject } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators, ValueChangeEvent } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 
 import { NotificationService } from '../../shared/services/notification.service';
 import { TodoStore } from '../../todos/store/todo.state';
-import { delay } from 'rxjs';
+import { delay, filter } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { TodoInterface } from '../todo.interface';
+import { patchState } from '@ngrx/signals';
 
 @Component({
   selector: 'app-todo',
@@ -16,7 +17,7 @@ import { TodoInterface } from '../todo.interface';
 })
 export class TodoComponent implements OnInit, OnDestroy, AfterViewInit {
   env = environment;
-  title= "Todo widget"
+  title = "Todo widget"
   readonly store = inject(TodoStore);
 
   progressBarVal = 0;
@@ -28,18 +29,30 @@ export class TodoComponent implements OnInit, OnDestroy, AfterViewInit {
     private formBuilder: FormBuilder
   ) {
     this.todoForm = this.formBuilder.group({
-      value:  new FormControl( this.store.currentTodo.value(), [Validators.required]),
-      done: new FormControl( this.store.currentTodo.done(), []),
+      value: new FormControl<string | null>(this.store.currentTodo.value(), [Validators.required]),
+      done: new FormControl<boolean>(this.store.currentTodo.done(), []),
     });
   }
 
   ngOnInit(): void {
     //Called once, when the instance is created.
-    this.titleService.setTitle(`${ this.title }`);
+    this.titleService.setTitle(`${this.title}`);
 
     setTimeout(() => {
       this.notificationService.openSnackBar('Welcome on Home page!');
     });
+
+    // To synchronize form update with signal store
+    this.todoForm.events
+      .pipe(filter((event) => event instanceof ValueChangeEvent)) // ValueChangeEvent | StatusChangeEvent | PristineChangeEvent | TouchedChangeEvent 
+      .subscribe({
+        next: (event: ValueChangeEvent<unknown>) => {
+          patchState(this.store, {
+            currentTodo: event.value,
+          });
+          console.log(event);
+        },
+      });
   }
 
   ngAfterViewInit(): void {
@@ -51,8 +64,8 @@ export class TodoComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   addTodo() {
-    this.store.addTodo(this.todoForm.value as unknown as TodoInterface ); // 'as unknown' is used for any Partial interface types
+    this.store.addTodo(this.todoForm.value as unknown as TodoInterface); // 'as unknown' is used for any Partial interface types
     // this.form.reset();
-    this.notificationService.openSnackBar('Form submitted');
+    this.notificationService.openSnackBar(`Form submitted: ${this.todoForm.value.value} | Current Todo state: ${this.store.currentTodo.value()}`);
   }
 }
